b91b0bb87f6a3701f5e2b5dd2c25dcd096461f24
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-05-25 14:37:45 +0530

    Checkpoint 1: Created Filter for wishes types, alongwith add more wishes functionality

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common'; 
 
+const filters = [
+  (item: WishItem) => item,
+  (item: WishItem) => !item.isComplete,
+  (item: WishItem) => item.isComplete
+]
 @Component({
   selector: 'app-root',
   imports: [CommonModule, FormsModule],
@@ -21,11 +25,24 @@ export class AppComponent {
 
   title = 'wishlist';
 
-  listFilter: String = '';
+  listFilter: any = '0';
 
   newWishText = '';
 
-  visibleItems : WishItem[] = this.items;
+  get visibleItems() : WishItem[] {
+    // Now we dont need this 
+
+    // let value = this.listFilter;
+
+    // if(value === '0') {
+    //   return this.items;
+    // } else if(value === '1'){
+    //   return this.items.filter(item => !item.isComplete);
+    // } else{
+    //   return this.items.filter(item => item.isComplete);
+    // }
+    return this.items.filter(filters[this.listFilter]);
+  };
 
   addNewWish() {
     this.items.push(new WishItem(this.newWishText));
@@ -43,12 +60,6 @@ export class AppComponent {
   //   console.log('The new value is', value);
   // }
   filterChanged(value: any) {
-    if(value === '0') {
-      this.visibleItems = this.items;
-    } else if(value === '1'){
-      this.visibleItems = this.items.filter(item => !item.isComplete);
-    } else{
-      this.visibleItems = this.items.filter(item => item.isComplete);
-    }
+    
   }
 }
