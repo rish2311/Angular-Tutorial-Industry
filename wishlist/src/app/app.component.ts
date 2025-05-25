1a7ef6d4449f2265eb4d989d6df6afb8ebe60ead
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-05-25 14:17:45 +0530

    Bind values to Form Controls and Listened to changes using NgModel

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
-import { CommonModule } from '@angular/common';
-import { AnyRecordWithTtl } from 'dns';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,12 +18,37 @@ export class AppComponent {
     new WishItem('Get Coffee', true),
     new WishItem('Find grass that cuts itself')
   ];
+
   title = 'wishlist';
+
+  listFilter: String = '';
+
+  newWishText = '';
+
+  visibleItems : WishItem[] = this.items;
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
 
   // toggleItem(e: any) {
   //  console.log(e) }
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
     console.log(item);
+  }
+
+  // filterChanged(value: any) {
+  //   console.log('The new value is', value);
+  // }
+  filterChanged(value: any) {
+    if(value === '0') {
+      this.visibleItems = this.items;
+    } else if(value === '1'){
+      this.visibleItems = this.items.filter(item => !item.isComplete);
+    } else{
+      this.visibleItems = this.items.filter(item => item.isComplete);
+    }
   }
 }
