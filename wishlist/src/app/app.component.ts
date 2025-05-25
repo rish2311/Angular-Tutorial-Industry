51cdeb1050b5958c07af7d9de3deb186269ffd83
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-05-25 13:31:46 +0530

    Property Binding(1 way Binding), Attribute and Event Binding (Event Listeners set up)

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
+import { AnyRecordWithTtl } from 'dns';
 @Component({
   selector: 'app-root',
   imports: [CommonModule],
@@ -9,11 +10,19 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  // Isse error to nhi aayega but data bhi nhi dikhega.
+  // Just niche wali line se error to nhi aayega but data bhi nhi dikhega agar hum new WishItems ko comment krde.
   items : WishItem[] = [
+
     new WishItem('To Learn Angular'),
     new WishItem('Get Coffee', true),
     new WishItem('Find grass that cuts itself')
   ];
   title = 'wishlist';
+
+  // toggleItem(e: any) {
+  //  console.log(e) }
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }
