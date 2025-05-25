9d7dd7f795e816e0b4450e1992a8d7aa961b2ed2
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-05-25 12:35:02 +0530

    created data models and iterating with ngFor directive

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,6 +13,6 @@ export class AppComponent {
     new WishItem('To Learn Angular'),
     new WishItem('Get Coffee', true),
     new WishItem('Find grass that cuts itself')
-  ]
+  ];
   title = 'wishlist';
 }
